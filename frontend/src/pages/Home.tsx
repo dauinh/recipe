698f1999e5c2e33ae9c4b07a11419e70d3d3d6b2
linh <linh.ha.nguyen20@gmail.com>
2024-04-10 15:19:27 -0400

    add tabs

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,20 +1,42 @@
 import { Flex, Spacer, Heading, Input, InputGroup, InputLeftElement } from '@chakra-ui/react'
+import { Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react'
 import { Search2Icon } from '@chakra-ui/icons'
 
 function Home() {
 
   return (
     <>
-      <Flex justify={'space-around'} align={'stretch'}>
-        <Heading as='h1' color='red.400'>RecipeBook</Heading>
+      <Flex justify={'space-around'} align={'stretch'} padding={'1rem 0.7rem 0.7rem 0.7rem'} w={'100%'} maxWidth={'1024px'}>
+        <Heading as='h1' size='lg' color='red.400' paddingRight={'1rem'}>RecipeBook</Heading>
         <Spacer />
         <InputGroup>
           <InputLeftElement pointerEvents='none'>
             <Search2Icon color='gray.300' />
           </InputLeftElement>
-          <Input variant='filled' placeholder='What are you cooking today?' />
+          <Input variant='filled' size='md' placeholder='I want to eat...' />
         </InputGroup>
       </Flex>
+      <Tabs variant='enclosed' colorScheme='red'>
+        <TabList>
+          <Tab _selected={{ bg: 'red.50' }}>One</Tab>
+          <Tab>Two</Tab>
+          <Tab>Three</Tab>
+        </TabList>
+
+        <TabPanels>
+          <TabPanel bgColor={'red.50'}>
+            <p>one!</p>
+          </TabPanel>
+
+          <TabPanel>
+            <p>two!</p>
+          </TabPanel>
+
+          <TabPanel>
+            <p>three!</p>
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
     </>
   )
 }
